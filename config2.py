53b61531e9095820a17fa82bca4400951428bfac
Jalenzzz <126164913+Jalenzzz@users.noreply.github.com>
2024-07-04 00:44:31 +0300

    Added configuration settings

---
+++ config2.py
@@ -0,0 +1,6 @@
+import os
+
+# basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config2:
+    SQLALCHEMY_DATABASE_URI="sqlite:////home/moringa/Moringa/Phase-4/flask/with_migrations/db.db"
